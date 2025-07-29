6e3c4cb8ab75b081611508fa5cb035f7bcc1b9ac
cloudrobo48 <hatakazuhiro.kyoto@gmail.com>
2025-07-29 16:34:22 +0900

    udpate test.py

--- tests/test_lambda_function.py
+++ tests/test_lambda_function.py
@@ -9,7 +9,7 @@ def test_lambda_handler():
             {
                 "name": "onamae",
                 "email": "onamae@example.com",
-                "message": "全力でやります！！",
+                "message": "めちゃ全力でやります！！",
             }
         )
     }
