7d0e8408d6919893d683a22674a365a6840e6647
cloudrobo48 <hatakazuhiro.kyoto@gmail.com>
2025-07-29 18:43:39 +0900

    update

--- tests/test_lambda_function.py
+++ tests/test_lambda_function.py
@@ -1,23 +1,41 @@
 import json
 
+import boto3
+from moto.ses import mock_ses
+
 from src import lambda_function
 
+# Lambda側で使ってる定数と合わせる（テスト用に同じ値にしておく）
+# SES の設定
+SES_SENDER = "hata.kazuhiro@fieldwork48.com"  # SESで登録済みの送信元メール
+SES_RECEIVER = "hata.kazuhiro@fieldwork48.com"  # 送信先メール
+SES_REGION = "ap-northeast-1"  # SESのリージョン
 
+
+@mock_ses
 def test_lambda_handler():
+    # SESモック空間を発生させる
+    client = boto3.client("ses", region_name=SES_REGION)
+
+    # SESに送信元メールアドレスを“検証済み”として登録（これが無いとモックでもエラー出る）
+    client.verify_email_identity(EmailAddress=SES_SENDER)
+
+    # Lambdaへ渡すイベント
     event = {
         "body": json.dumps(
             {
                 "name": "onamae",
-                "email": "onamae@example.com",
+                "email": SES_RECEIVER,
                 "message": "めちゃ全力でやります！",
             }
         )
     }
     context = None
 
+    # Lambda関数を実行
     response = lambda_function.lambda_handler(event, context)
 
-    # 基本構造の検証
+    # レスポンスの構造確認
     assert "statusCode" in response
     assert response["statusCode"] == 200
 
